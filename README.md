241fa29a985b1018067ac5090e21ea75568e9aa3
kevinlaiGH <kevin.km.lai@gmail.com>
2023-05-23 16:53:47 +1000

    🌴Created Board and Cell component

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # react-tetris
+
+Todos:
+
+- [ ] Create a 10 by 20 grid that we wee each cell that going to be gray out initially 
+- [ ] When we place blocks, each cell will change from gray to other colors
+- [ ] 
+- [ ] 
+
---
+++ src/components/Board.tsx
@@ -0,0 +1,30 @@
+import { BoardShape } from "../types";
+import Cell from "./Cell";
+
+interface Props {
+  currentBoard: BoardShape;
+}
+
+function Board({ currentBoard }: Props) {
+  return (
+    <div className="board">
+      {currentBoard.map((row, rowIndex) => (
+        <div className="row" key={`${rowIndex}`}>
+          {row.map((cell, colIndex) => (
+            <Cell key={`${rowIndex}-${colIndex}`} type={cell} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Board;
+
+/* 
+Map over our current board props and 
+Create a new row for each row in the board (give each row a unique key)
+Within each row, we can create a cell for each value
+Create a new component cell to take in the type of cell (cell options we created earlier in types.tsx)
+
+*/
